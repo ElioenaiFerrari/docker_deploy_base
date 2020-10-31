3b7f76f5394e3d2997b1622c9439d323462cf215
elioenaiferrari <elioenaiferrari@gmail.com>
2020-10-31 17:39:55 -0300

    update

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -6,9 +6,8 @@ Mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-});
-
-Mongoose.connection.on('error', () => console.error('[ERROR]'));
-Mongoose.connection.once('open', () => console.log(`[SUCCESS]`));
+})
+  .then(console.log)
+  .catch(console.error);
 
 export default Mongoose;
