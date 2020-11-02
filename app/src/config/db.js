1329b1fe53109879de6b99fd6f365ba46a054786
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-02 16:51:32 -0300

    update

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -2,10 +2,10 @@ import Mongoose from 'mongoose';
 
 const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS } = process.env;
 
-const conURL = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const conURL = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:27017/${DB_NAME}?authSource=admin`;
 
 const feedback = (status) => (data) => {
-  console.log(`[${status}] in ${conURL}`);
+  console.log(`[${status}] in ${conURL} -> ${data}`);
 };
 
 Mongoose.connect(conURL, {
