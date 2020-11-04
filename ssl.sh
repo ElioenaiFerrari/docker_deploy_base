8ae5b81e274647cd31778d53511e342020dda377
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-03 21:55:13 -0300

    update

--- ssl.sh
+++ ssl.sh
@@ -13,7 +13,7 @@ fi
 APP_HOST=$(cat .env | grep APP_HOST | cut -d= -f2)
 search_host=$(cat /etc/hosts | grep $APP_HOST)
 
-if [ -z $search_host]
+if [ -z $search_host ]
 then
   echo "registering host..."
   sed -i "/127.0.0.1/a127.0.0.1\t$APP_HOST" /etc/hosts
