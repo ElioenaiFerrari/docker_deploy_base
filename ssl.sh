7babf6a2e5528911ee4f86509b537e93f391b1d3
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-03 21:13:57 -0300

    update

--- ssl.sh
+++ ssl.sh
@@ -7,17 +7,16 @@ if [ -z $certificate ]
 then
   echo "building ssl..."
   make ssl
-
-  APP_HOST=$(cat .env | grep APP_HOST | cut -d= -f2)
-
-  search_host=$(cat /etc/hosts | grep $APP_HOST)
-
-  if [ -z $search_host]
-  then
-    echo "registering host..."
-    sed -i "/127.0.0.1/a127.0.0.1\t$APP_HOST" /etc/hosts
-  fi
-  
   echo "building complete."
 fi
+
+APP_HOST=$(cat .env | grep APP_HOST | cut -d= -f2)
+search_host=$(cat /etc/hosts | grep $APP_HOST)
+
+if [ -z $search_host]
+then
+  echo "registering host..."
+  sed -i "/127.0.0.1/a127.0.0.1\t$APP_HOST" /etc/hosts
+fi
+  
 
